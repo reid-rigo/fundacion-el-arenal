e8faec8603446f6079217d86ab1f57e99879c966
Reid Lynch <reid.lynch+rigoleto@gmail.com>
2013-02-17 14:46:08 -0500

    basic footer

--- wp-content/themes/simple-catch/footer.php
+++ wp-content/themes/simple-catch/footer.php
@@ -9,9 +9,26 @@
 ?>
 	<div id="footer">
     <div class="layout-978">
-    	<div class="col7 copyright no-margin-left">
+    	<div class="col9 copyright no-margin-left">
 				 © 2013 Fundación El Arenal
 			</div>
+
+			<div class="vcard col3">
+				<a class="url fn n" href="http://fundacionelarenal.org"></a>
+				<address>
+					<div class="org">Fundación El Arenal</div>
+				 	<div class="adr">
+				  	<div class="street-address">Ramona Cordero y Leon 3-92</div>
+				  	<span class="locality">Cuenca</span>, 
+					  <span class="region">Azuay</span>
+					  <span class="country-name">Ecuador</span>
+				 	</div>
+				 	<div class="tel">(593) 07 409 3674</div>
+				</address>
+			</div>
+
+			<div class="row-end"></div>
+
 		</div><!-- .layout-978 -->
 	</div><!-- #footer -->      
 <?php wp_footer(); ?>
